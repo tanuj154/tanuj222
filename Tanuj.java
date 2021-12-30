6a582b40e507c9985487f08f360547282e51b51d
tanuj154 <91303364+tanuj154@users.noreply.github.com>
2021-12-30 20:02:50 +0530

    Add files via upload

---
+++ Tanuj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+public class Tanuj 
+{
+    public class static void main(string args[])
+   {
+    system.out.println("HelloWorld");
+   }
+}
